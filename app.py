f64c002d2849a35a3813e9ba1523ec9b6f0e9e0e
Eddie Hale <eddie.hale@unqork.com>
2022-08-24 20:17:16 -0500

    updated with error handling on contact form

--- app.py
+++ app.py
@@ -9,27 +9,27 @@ def get_current_time():
   now = datetime.now()
   return now.strftime("%m/%d/%Y, %H:%M")
 
-# def send_email(first, last, subject, message):
-#   # Open a plain text file for reading.  For this example, assume that
-#   # the text file contains only ASCII characters.
-#   # with open(textfile, 'rb') as fp:
-#   #     # Create a text/plain message
-#   #     msg = MIMEText(fp.read())
+def send_email(first, last, subject, message):
+  # Open a plain text file for reading.  For this example, assume that
+  # the text file contains only ASCII characters.
+  # with open(textfile, 'rb') as fp:
+  #     # Create a text/plain message
+  #     msg = MIMEText(fp.read())
 
-#   msg = MIMEText(message)
+  msg = MIMEText(message)
 
-#   # me == the sender's email address
-#   # you == the recipient's email address
-#   me = 'no-reply@gmail.com'
-#   you = 'eddie.hale@gmail.com'
-#   msg['Subject'] = 'The contents of %s' % subject
-#   msg['From'] = me
-#   msg['To'] = you
+  # me == the sender's email address
+  # you == the recipient's email address
+  me = 'no-reply@gmail.com'
+  you = 'eddie.hale@gmail.com'
+  msg['Subject'] = 'The contents of %s' % subject
+  msg['From'] = me
+  msg['To'] = you
 
-#   # Send the message via our own SMTP server, but don't include the
-#   # envelope header.
-#   s = smtplib.SMTP('localhost')
-#   s.sendmail(me, [you], msg.as_string())
-#   s.quit()
+  # Send the message via our own SMTP server, but don't include the
+  # envelope header.
+  s = smtplib.SMTP('localhost')
+  s.sendmail(me, [you], msg.as_string())
+  s.quit()
 
 # send_email('Eddie', 'Hale', 'Test email from laptop', 'Hello world!!')
